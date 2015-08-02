ab46097c5b6349503d0ede53cf3ab8b4f6d3a20a
giovanifss <11573379+giovanifss@users.noreply.github.com>
2015-08-01 21:29:34 -0300

    Solving test issues

--- spec/password_control_spec.rb
+++ spec/password_control_spec.rb
@@ -6,37 +6,37 @@ describe PasswordControl do
 		# Default algorithm = Bcrypt
 		it "Default password test" do
 			@passControl = PasswordControl::PassControl.new :secret => "A really strong password"
-			expect(@hashControl.prepareForSave[:algorithm]).to eql("bcrypt")
+			expect(@passControl.prepareForSave[:algorithm]).to eql("bcrypt")
 		end
 
 		# Setting Scrypt directly
 		it "Setting scrypt algorithm test" do
 			@passControl = PasswordControl::PassControl.new :secret => "password", :algorithm => "scrypt"
-			expect(@hashControl.prepareForSave[:algorithm]).to eql("scrypt")
+			expect(@passControl.prepareForSave[:algorithm]).to eql("scrypt")
 		end
 
 		# Setting MD5 directly
 		it "Setting md5 algorithm test" do
 			@passControl = PasswordControl::PassControl.new :secret => "test", :algorithm => "mD5" # Whatever if lowercase or uppercase
-			expect(@hashControl.prepareForSave[:algorithm]).to eql("md5")
+			expect(@passControl.prepareForSave[:algorithm]).to eql("md5")
 		end
 
 		# Setting sha256 directly
 		it "Setting sha256 algorithm test" do
 			@passControl = PasswordControl::PassControl.new :secret => "Im running out of ideas", :algorithm => "ShA256"
-			expect(@hashControl.prepareForSave[:algorithm]).to eql("sha256")
+			expect(@passControl.prepareForSave[:algorithm]).to eql("sha256")
 		end
 
 		# Setting sha384 directly
 		it "Setting sha384 algorithm test" do
 			@passControl = PasswordControl::PassControl.new :secret => "Give me and idea of password", :algorithm => "sHa384"
-			expect(@hashControl.prepareForSave[:algorithm]).to eql("sha384")
+			expect(@passControl.prepareForSave[:algorithm]).to eql("sha384")
 		end
 
 		# Setting sha512 directly
 		it "Setting sha512 algorithm test" do
 			@passControl = PasswordControl::PassControl.new :secret => "Open source o/", :algorithm => "Sha512"
-			expect(@hashControl.prepareForSave[:algorithm]).to eql("sha512")
+			expect(@passControl.prepareForSave[:algorithm]).to eql("sha512")
 		end
 
 	end
